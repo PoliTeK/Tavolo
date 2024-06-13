7cb6d6660fd16c29042c94695cc6f9e28bf6730f
Rexus752 <t.rexus752@gmail.com>
2024-06-13 18:51:42 +0200

    Update osc01.ino

--- Beta_V0/osc01.ino
+++ Beta_V0/osc01.ino
@@ -61,7 +61,13 @@ const IntMap CLIP_map(0,4096,70,255);
 const IntMap LEDCLIP_map(50,255,255,0);
 const IntMap GAIN_map(70,255,65,20);
 
- 
+
+//--------------------------------------------Pulsante-------------------------------------------------
+bool Switch_RM=false;
+unsigned long debounceDelay = 50; // Ritardo di debounce in millisecondi
+bool buttonState = false; // Variabile booleana da invertire
+bool lastButtonState = false; // Stato precedente del pulsante
+unsigned long lastDebounceTime = 0; // Tempo dell'ultimo cambiamento di stato del pulsante
 
 
 //--------------------------------------------Oscillatori Voci-------------------------------------------
@@ -72,6 +78,10 @@ Oscil <2048, AUDIO_RATE> oscillatori[] = {Osc0, Osc1};
 int Sum = 0;
 
 
+//--------------------------------------------Oscillatori Modulazioni-------------------------------------------
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> Carrier2(SIN2048_DATA); // portante per Ring Modulatin(RM)  Osc1 
+Oscil <SIN2048_NUM_CELLS, AUDIO_RATE> Carrier1(SIN2048_DATA);
+
 
 //---------------------------------------Filtri-------------------------------------------------
 ResonantFilter<LOWPASS, uint16_t> lpf1; 
@@ -80,12 +90,13 @@ int cutoff1 = 0;
 int cutoff2 = 0;
 int resonance=0;
 
+
 //---------------------------------------Distorsione-------------------------------------------------
 byte gain=255;
 int clip=255;
 
 
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------
 void setup() {
   //Serial.begin(9600);
   pinMode(LEDCLIP_PIN,OUTPUT);
@@ -93,8 +104,32 @@ void setup() {
   
 }
 
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-void updateControl() { 
+//-------------------------------------------------------------------------------------------------------------------
+void updateControl() {
+
+//--------------------------------------------Debouncing--------------------------------------------------------
+  int reading = digitalRead(P0_PIN); // Legge lo stato attuale del pulsante
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        Switch_RM = !Switch_RM;
+      }
+    }
+  }
+  //Serial.println(Switch_RM);
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
+  // Pippone finito ;)
 
 //----------------------------------------Note Voci----------------------------------------------------------- 
   // Vettore con tutti i valori letti dai potenziometri
@@ -111,6 +146,29 @@ void updateControl() {
   Osc0.setFreq(frequenze_base[0]*2);
   Osc1.setFreq(frequenze_base[1]*4);
 
+//-------------------------------------------------------RM-------------------------------------------------------------------------
+  float Carrier2_Freq = MOD_map(GEN_map(mozziAnalogRead(POT5_PIN))); // leggo il potenziometro per determinare la frequenza aggiuntiva
+  
+  // portante1: è un'ottava sotto il segnale modulato + la frequenza letta dal potenziometro
+  // (divido per 8 per avere più precisione)
+  Carrier1.setFreq(frequenze_base[1]*48+Carrier1_Freq/8);
+  
+  // portante2: è alla stessa ottava del segnale modulato + la frequenza letta dal potenziometro
+  // (divido per 10 per avere una precisione diversa dall'altra portante)
+  Carrier2.setFreq(frequenze_base[0]*24+Carrier2_Freq/10);
+  
+  // modo simpatico per fare oscillare il led a tempo con la modulazione
+  if(Switch_RM==true){  
+    if(Carrier1.next()<0){
+      analogWrite(LEDM_PIN,0);
+    }
+    if(Carrier2.next()<0){
+      analogWrite(LEDM_PIN,0);
+    }
+    else analogWrite(LEDM_PIN,Carrier1.next());   
+  }
+  else analogWrite(LEDM_PIN,0);
+
 //------------------------------------------------------------------------Filtri------------------------------------------------
   cutoff1 = CUTOFF_map(mozziAnalogRead(POT2_PIN));
   cutoff2 = CUTOFF_map(mozziAnalogRead(POT3_PIN));
@@ -121,7 +179,7 @@ void updateControl() {
   //Serial.println(cutoff2);
 
 //------------------------------------------------------------------------Distorsione------------------------------------------------
-  clip = CLIP_map(mozziAnalogRead(POT4_PIN));
+  clip = CLIP_map(mozziAnalogRead(POT5_PIN));
   analogWrite(LEDCLIP_PIN,LEDCLIP_map(clip)); // Da quello che ho capito analogWrite non si può usare
   gain = GAIN_map(clip);
 
@@ -131,13 +189,13 @@ void updateControl() {
 
 
 AudioOutput_t updateAudio() {
-  Sum = ( (lpf1.next(Osc0.next())*4) + (lpf2.next(Osc1.next())*7) ); // somma i due osc con guadagni diversi
-  Sum = MonoOutput::fromAlmostNBit(13,Sum); // ricentro il segnale su 8 bit
-  // Gain e clip sono opposti, il potenziometro 4 controlla la soglia del Clip e più bassa è la soglia più il volume è alto 
-  if (Sum>clip-50) Sum=clip-50;
-  else if (Sum<(-clip)) Sum=-clip;
-  Sum = MonoOutput::from16Bit(Sum*gain).clip(); 
-  return MonoOutput::from8Bit(Sum); 
+  if (Switch_RM==true){
+    Sum = ( ((lpf1.next((Carrier2.next()>>9)*Osc0.next()))*4) + ((lpf2.next(((Carrier1.next()>>9))*Osc1.next())*3 )) )>>5;
+    // moltiplico il segnale per le 2 portanti divise (di molto) secondo il gusto per rendere la modulazione il giusto invasiva
+  }
+  else{
+}
+  return MonoOutput::fromAlmostNBit(8,Sum);
 }
 
 void loop() {
